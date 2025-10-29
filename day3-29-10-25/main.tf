b036a82ee59d78ed90cf3ffb6d915eaea7623fa0
ACE <rakshak28anirudh@gmail.com>
2025-10-29 17:37:36 +0530

    day3

---
+++ day3-29-10-25/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "name" { 
+    instance_type = var.type
+     ami = var.ami_id
+     tags = {
+       Name = "prod"
+     }
+
+
+  
+}
+
+
+
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+  
+}
---
+++ day3-29-10-25/s3.tf
@@ -0,0 +1,5 @@
+resource "aws_s3_bucket" "name" {
+    bucket = "ace-12-terra-bucket"
+    
+  
+}
