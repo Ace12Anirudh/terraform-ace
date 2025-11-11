2d897d62a034f5fd164c42d63b5132f59ddf153c
ACE <rakshak28anirudh@gmail.com>
2025-11-11 23:01:09 +0530

    locals

---
+++ day10-rds-nullresource/main.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# Create the RDS instance
+resource "aws_db_instance" "mysql_rds" {
+  identifier              = "my-mysql-db"
+  engine                  = "mysql"
+  instance_class          = "db.t3.micro"
+  username                = "admin"
+  password                = "Password123!"
+  db_name                 = "dev"
+  allocated_storage       = 20
+  skip_final_snapshot     = true
+  publicly_accessible     = true
+}
+
+# Use null_resource to execute the SQL script from your local machine
+# resource "null_resource" "local_sql_exec" {
+#   depends_on = [aws_db_instance.mysql_rds]
+
+#   provisioner "local-exec" {
+#     command = "mysql -h ${aws_db_instance.mysql_rds.address} -u admin -pPassword123! dev < init.sql"
+#   }
+
+#   triggers = {
+#     always_run = timestamp()
+#   }
+# }
+
+# Example EC2 instance (replace with yours if already existing)
+resource "aws_instance" "sql_runner" {
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  key_name               = "task"                # Replace with your key pair name
+  associate_public_ip_address = true
+  user_data = "sudo yum install mariadb105-server -y"
+
+  tags = {
+    Name = "SQL Runner"
+  }
+}
+
+# Deploy SQL remotely using null_resource + remote-exec
+resource "null_resource" "remote_sql_exec" {
+  depends_on = [aws_db_instance.mysql_rds, aws_instance.sql_runner]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("my-keypair")   # Replace with your PEM file path
+    host        = aws_instance.sql_runner.public_ip
+  }
+
+#   provisioner "file" {
+#     source      = "init.sql"
+#     destination = "/tmp/init.sql"
+#   }
+
+  provisioner "remote-exec" {
+    inline = [
+      #"mysql -h ${aws_db_instance.mysql_rds.address} -u ${jsondecode(aws_secretsmanager_secret_version.rds_secret_value.secret_string)["username"]} -p${jsondecode(aws_secretsmanager_secret_version.rds_secret_value.secret_string)["password"]} < /tmp/init.sql"
+      "mysql -h ${aws_db_instance.mysql_rds.address} -u admin -pPassword123!"
+    ]
+  }
+
+  triggers = {
+    always_run = timestamp() #trigger every time apply 
+  }
+}
+
+
+
+
+# ADD RDS creation script only accessbale interanlly si disable public access 
+# Remote provisioner server also should create insame vpc 
+# enable secrets fro secret manager and call secrets into RDS for this process vpc endpoint is require or nat gateway is required to access secrets to rds internall as secremanger is not in side VPC sefrvice 
