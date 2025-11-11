3c77eac459030a3468c2cf532da4511fdb23e6eb
ACE <rakshak28anirudh@gmail.com>
2025-11-11 20:25:36 +0530

    day10

--- day9-userdata/test.sh
+++ day9-userdata/test.sh
@@ -5,3 +5,11 @@ sudo service httpd start
 sudo systemctl enable httpd
 echo "<h1>AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
 yum install git -y
+
+
+
+
+
+
+
+
