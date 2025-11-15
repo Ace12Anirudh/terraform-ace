559f3b7b836615eebf2f8abed177f6bf69d131b5
ACE <rakshak28anirudh@gmail.com>
2025-11-15 16:09:03 +0530

    day13

---
+++ day13-root-child-modules/modules/ec2/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_1_id
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
